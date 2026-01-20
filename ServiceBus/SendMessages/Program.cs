8c5a9b7e4ec62dc4c4d91a79d05e7c0f1c341d35
mayanksharmaade <mayankcst@gmail.com>
2026-01-20 16:17:51 +1030

    Add files via upload

---
+++ ServiceBus/SendMessages/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Messaging.ServiceBus;
+
+const   string BusConnectionstring= "Endpoint=sb://mayankservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ERuAplr7wxWBmWTxvUmGXTuASJqm8SrH3+ASbFv8fgs=";
+const string QueueName = "mayankservicebus-queue-1";
+
+const int MaxNumberofMessages = 5;
+
+ServiceBusClient client;
+ServiceBusSender sender;
+
+client = new ServiceBusClient(BusConnectionstring);
+sender = client.CreateSender(QueueName);
+
+using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+for (int i = 1; i <= MaxNumberofMessages; i++)
+{
+    if (!messageBatch.TryAddMessage(new ServiceBusMessage($"This Message is-{i}")))
+    {
+        Console.WriteLine($"Message-{i} was not added ");
+    }
+}
+
+try {
+    await sender.SendMessagesAsync(messageBatch);
+    Console.WriteLine("Messages have been sent ");
+
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine("Exception occured:" + ex.Message);
+}
+finally
+{
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
