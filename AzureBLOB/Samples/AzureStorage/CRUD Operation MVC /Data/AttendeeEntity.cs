758cc2eef54849255543c346655007f7337fd785
mayanksharmaade <mayankcst@gmail.com>
2026-01-20 23:26:02 +1030

    Add files via upload

---
+++ AzureBLOB/Samples/AzureStorage/CRUD Operation MVC /Data/AttendeeEntity.cs
@@ -0,0 +1,19 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace AzureBLOBsample.Data
+{
+    public class AttendeeEntity : ITableEntity
+    {
+         public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email {get;set;}
+
+        public string Profession { get; set; }
+        public string PartitionKey { get ; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+}
