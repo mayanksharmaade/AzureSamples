22a23427a39c4ea1ee9b74c857c51cb1cd704722
mayanksharmaade <mayankcst@gmail.com>
2026-01-20 23:20:35 +1030

    Add files via upload

---
+++ AzureBLOB/Samples/AzureStorage/CRUD Operation MVC /Services/ITablestorageService.cs
@@ -0,0 +1,14 @@
+﻿using AzureBLOBsample.Data;
+
+namespace AzureBLOBsample.Services
+{
+    public interface ITablestorageService
+    {
+        Task<AttendeeEntity> GetAttendee(string Profession, string id);
+        Task<List<AttendeeEntity>> GetAttendees();
+
+        Task AddAttendee(AttendeeEntity attend);
+
+        Task DeleteAttendee(string Profession, string id);
+    }
+}
---
+++ AzureBLOB/Samples/AzureStorage/CRUD Operation MVC /Services/TableStorageService.cs
@@ -0,0 +1,51 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using AzureBLOBsample.Data;
+using System.Reflection.Metadata;
+
+namespace AzureBLOBsample.Services
+{
+    public class TableStorageService:ITablestorageService
+    {
+        private readonly IConfiguration config;
+        private const string Tablename = "Attendees";
+        public TableStorageService( IConfiguration _config)
+        {
+            config = _config;
+        }
+        public  async Task<AttendeeEntity> GetAttendee( string Profession,string id)
+        {
+            var tableclient = await GetTableClient();
+            return await tableclient.GetEntityAsync<AttendeeEntity>(Profession, id);
+        }
+
+        public  async Task<List<AttendeeEntity>> GetAttendees()
+        {
+            var tableclient = await GetTableClient();
+            Pageable<AttendeeEntity> Attendees = tableclient.Query<AttendeeEntity>();
+            return Attendees.ToList();
+        }
+
+        public async  Task AddAttendee(AttendeeEntity attend)
+        {
+            var tableclient = await GetTableClient();
+            await tableclient.UpsertEntityAsync(attend);
+        }
+        public async Task DeleteAttendee(string Profession, string id)
+        {
+
+            var tableclient = await GetTableClient();
+            await tableclient.DeleteEntityAsync(Profession, id);
+
+        }
+
+        private async Task <TableClient> GetTableClient()
+        {
+            var serviceclient = new TableServiceClient(config["StorageConnectionStrings"]);
+            var tableclient = serviceclient.GetTableClient(Tablename);
+            await tableclient.CreateIfNotExistsAsync();
+
+            return tableclient;
+        }
+    }
+}
