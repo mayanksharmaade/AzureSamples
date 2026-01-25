82eef97b383add6b937de4be09cfee79805d7de7
mayanksharmaade <mayankcst@gmail.com>
2026-01-25 18:54:17 +1030

    Integrate blob storage for attendee image uploads
    Added blob storage service integration for image handling.

--- AzureBLOB/Samples/AzureStorage/CRUD Operation MVC /Controllers/AttendeesRegistrationController.cs
+++ AzureBLOB/Samples/AzureStorage/CRUD Operation MVC /Controllers/AttendeesRegistrationController.cs
@@ -1,4 +1,4 @@
-﻿using AzureBLOBsample.Data;
+using AzureBLOBsample.Data;
 using AzureBLOBsample.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,13 +10,22 @@ namespace AzureBLOBsample.Controllers
     {
         // GET: AttendeesRegistrationController
         private readonly ITablestorageService _tablestorage;
-        public AttendeesRegistrationController(ITablestorageService service)
+        private readonly IBlobStorageService _blobstorage;
+        public AttendeesRegistrationController(ITablestorageService service,IBlobStorageService blobstorage)
         {
-            _tablestorage = service;
+           this. _tablestorage = service;
+            this._blobstorage = blobstorage;
         }
         public  async Task<ActionResult> Index()
         {
             var data = await _tablestorage.GetAttendees();
+             foreach ( var item in data)
+            {
+
+                if (!string.IsNullOrEmpty(item.ImageName))
+                { item.ImageName = await _blobstorage.GetBlobUrl(item.ImageName); }
+                 
+            }
             return View(data);
         }
 
@@ -37,12 +46,17 @@ namespace AzureBLOBsample.Controllers
         // POST: AttendeesRegistrationController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create(AttendeeEntity Attend)
+        public async Task<ActionResult> Create(AttendeeEntity Attend, IFormFile file)
         {
             try
             {
+                var id = Guid.NewGuid().ToString();
                 Attend.PartitionKey = Attend.Profession;
-                Attend.RowKey = Guid.NewGuid().ToString();
+                Attend.RowKey = id;
+                if (file != null && file.Length > 0)
+                {
+                    Attend.ImageName = await _blobstorage.UploadBlob(file, id);
+                }
 
                 await _tablestorage.AddAttendee(Attend);
                 return RedirectToAction(nameof(Index));
@@ -92,7 +106,9 @@ namespace AzureBLOBsample.Controllers
         {
             try
             {
+                var data = await _tablestorage.GetAttendee(Profession, id);
                 await _tablestorage.DeleteAttendee(Profession, id);
+                await _blobstorage.RemoveBlob(data.ImageName);
                 return RedirectToAction(nameof(Index));
             }
             catch
