875f7c0005fa7a594b6e90b54a19eeb70dcbea75
mayanksharmaade <mayankcst@gmail.com>
2026-01-25 18:58:20 +1030

    Update Program.cs

--- AzureBLOB/Samples/AzureStorage/Program.cs
+++ AzureBLOB/Samples/AzureStorage/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<ITablestorageService, TableStorageService>();
+builder.Services.AddScoped<IBlobStorageService, BlobStorageService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
