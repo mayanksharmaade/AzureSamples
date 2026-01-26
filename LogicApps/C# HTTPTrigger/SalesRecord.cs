a7de768c9d4711e3cfd9258627dbdd6915baaeaa
mayanksharmaade <mayankcst@gmail.com>
2026-01-26 16:27:30 +1030

    Add SalesRecord model for sales data

---
+++ LogicApps/C# HTTPTrigger/SalesRecord.cs
@@ -0,0 +1,10 @@
+namespace SalesDataDemo.Models
+{
+    public class SalesRecord
+    {
+        public string ItemType { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+
+    }
+}
